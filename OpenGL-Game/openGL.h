e747339a51eb475bcef44637eb2b191ec110ea56
LuckiiGoggy <luck-13@live.ca>
2015-04-21 14:47:22 -0700

    Base from Rene

---
+++ OpenGL-Game/openGL.h
@@ -0,0 +1,12 @@
+#ifndef OPENGL_H
+#define OPENGL_H
+
+#define NDEBUG
+#define FREEGLUT_STATIC
+#define _LIB
+//FREEGLUT_LIB_PRAGMAS=0
+
+#include "Dependencies\glew\glew.h"
+#include "Dependencies\freeglut\freeglut.h"
+
+#endif
