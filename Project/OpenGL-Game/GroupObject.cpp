c39882861ab5094fb1358efb236ae2fdc9d10f28
LuckiiGoggy <luck-13@live.ca>
2015-04-22 21:57:07 -0700

    Setting up GroupObject
    Still under construction, currently won't do anything

---
+++ Project/OpenGL-Game/GroupObject.cpp
@@ -0,0 +1,53 @@
+#include "GroupObject.h"
+
+
+GroupObject::GroupObject()
+{
+}
+
+
+GroupObject::~GroupObject()
+{
+}
+
+
+
+void GroupObject::Render()
+{
+
+}
+
+void GroupObject::Move(int x, int y, int z)
+{
+
+}
+
+void GroupObject::MoveUp(int delta)
+{
+	
+}
+
+void GroupObject::MoveDown(int delta)
+{
+
+}
+
+void GroupObject::MoveLeft(int delta)
+{
+
+}
+
+void GroupObject::MoveRight(int delta)
+{
+
+}
+
+void GroupObject::MoveForward(int delta)
+{
+
+}
+
+void GroupObject::MoveBackward(int delta)
+{
+
+}
