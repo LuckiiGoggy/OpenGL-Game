f9144067f293bcd460b3ebce369da185c727ab46
LuckiiGoggy <luck-13@live.ca>
2015-04-23 00:07:54 -0700

    re-updating openGL base

--- Project/OpenGL-Game/openGL.h
+++ Project/OpenGL-Game/openGL.h
@@ -1,10 +1,7 @@
 #ifndef OPENGL_H
 #define OPENGL_H
 
-#define NDEBUG
-#define FREEGLUT_STATIC
-#define _LIB
-//FREEGLUT_LIB_PRAGMAS=0
+#define FREEGLUT_LIB_PRAGMAS 0
 
 #include "Dependencies\glew\glew.h"
 #include "Dependencies\freeglut\freeglut.h"
