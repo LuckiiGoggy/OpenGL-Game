d57e6e4d359ad3a825c364f16acd8b72589eec38
LuckiiGoggy <luck-13@live.ca>
2015-04-27 10:34:42 -0700

    Create Character Class
    Basic Information and control functionality of a Character(ie the
    Player, and AI, etc)

---
+++ Project/OpenGL-Game/Character.h
@@ -0,0 +1,54 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <string>
+#include <map>
+#include "Dependencies\glm\glm\glm.hpp"
+
+#include "GroupObject.h"
+#include "IUpdateable.h"
+
+/**Character Class is a container for the GroupObject that makes up
+	a character.
+	It provides the interface for interacting with a character.**/
+class Character: public IUpdateable
+{
+public:
+	Character();
+	~Character();
+	virtual void Act(std::string funcName);
+	
+	virtual void MoveUp(void);
+	virtual void MoveDown(void);
+	virtual void MoveLeft(void);
+	virtual void MoveRight(void);
+	virtual void MoveForward(void);
+	virtual void MoveBackward(void);
+
+	virtual void SetMovementSpeed(int speed);
+	virtual int  GetMovementSpeed();
+
+	virtual void Update(float timeDelta);
+
+	virtual void AddMember(std::string, GroupObject *);
+	virtual void RemoveMember(std::string);
+
+protected:
+	/**Collection of actions available to all characters.**/
+	/**Note: when extending Character create a new collection and modify
+		the Act function to read through your collection as well**/
+	std::map<std::string, void(Character::*)(void)> baseActions;
+
+	/**Collection of GroupObjects that make up the Character**/
+	std::map<std::string, GroupObject *> members;
+
+	int movementSpeed;
+	glm::vec3 position;
+	glm::vec3 moveDelta;
+
+	virtual void MoveMembers();
+	
+	
+};
+
+#endif
