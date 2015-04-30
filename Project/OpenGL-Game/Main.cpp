0990fb14cd59dcf255eed23bc49532a3e8843f33
rene-ye <rene.ye@hotmail.com>
2015-04-30 15:38:35 -0700

    Created a Spear

--- Project/OpenGL-Game/Main.cpp
+++ Project/OpenGL-Game/Main.cpp
@@ -23,11 +23,11 @@ int main(int argc, char **argv) {
 	int menu;
 	glutInit(&argc, argv);
 	GlutManager::Init();
-	/*char* obj_filename = (char*) "../Assets/Models/wallCube.obj";
+	char* obj_filename = (char*) "../Assets/Models/spear.obj";
 	char* v_shader_filename = (char*) "../Assets/Shaders/gouraud-shading-two-sided.v.glsl";
 	char* f_shader_filename = (char*) "../Assets/Shaders/gouraud-shading-two-sided.f.glsl";
 
-	myMesh.Init(obj_filename, v_shader_filename, f_shader_filename);*/
+	myMesh.Init(obj_filename, v_shader_filename, f_shader_filename);
 
 
 	GlutManager::AddMember("MyMesh", &myMesh);
