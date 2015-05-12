b32b93ffb2cec6a3c9c69ae91d5e12afcba5399a
LuckiiGoggy <luck-13@live.ca>
2015-05-12 16:30:42 -0700

    Cleaning Up Main

--- Project/OpenGL-Game/Main.cpp
+++ Project/OpenGL-Game/Main.cpp
@@ -2,22 +2,8 @@
 #include "openGL.h"
 #include <gl/GLU.h>
 
-#include "WorldEngine.h"
-#include "InputManager.h"
-#include "IGameObject.h"
-#include "GroupObject.h"
-#include "TestObject.h"
-#include "MeshObject.h"
 #include "GlutManager.h"
 #include "GameScene.h"
-#include "Player.h"
-
-WorldEngine engine = WorldEngine();
-void updateGame();
-void menuEvents(int choice);
-void renderScene(void);
-
-GroupObject test;
 
 int main(int argc, char **argv) {
 	glutInit(&argc, argv);
@@ -26,29 +12,10 @@ int main(int argc, char **argv) {
 
 	
 	GameScene *gS = new GameScene();
-
 	gS->Init();
-
 	GlutManager::AddMember("Derp", gS);
-
-	
-
-
 
 	GlutManager::StartLoop();
 
 	return 0;
-}
-
-
-void renderScene(void) {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(0.5, 0.5, 0.5, 1.0);
-
-	if (engine.loaded == true) {
-		engine.renderWorld();
-	}
-
-
-	glutSwapBuffers();
 }
